2d1f9dd9eeb3dabb606a6e595396f97b2d9a9c52
Cherviatsova <chervyatsovaelena2@gmail.com>
2022-01-21 18:03:20 +0200

    Updates

--- my-app/src/FriendList/FriendList.styled.jsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const FriendsList = styled.ul`
-    margin: 0 auto;
-    padding-right: 30px;
-    padding-bottom: 15px;
-    width: 300px;
-    list-style: none
-`;
